977305576a842c48a25512d42a5e63750c984ce6
U220053 <knitu7392@gmail.com>
2024-04-22 19:55:40 +0530

    final app as on 22nd

--- app/Redeem/index.tsx
+++ app/Redeem/index.tsx
@@ -1,3 +1,4 @@
+/*
 "use client";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -89,7 +90,7 @@ export default function Redeem() {
         ) : (
           <div className=" h-[30rem] ">
             <Image src={gif} alt="home" />
-            <p className="text-amber-400 font-mono text-2xl font-extrabold text-center mt-2">
+            <p className="text-lightyellow font-mono text-2xl font-extrabold text-center mt-2">
               Token ID:{apiResponse}
             </p>
           </div>
@@ -103,7 +104,7 @@ export default function Redeem() {
           alt="home"
           className="overflow-hidden"
         />
-        <div className="text-amber-400 text-2xl md:text-6xl font-bold title-text mt-4 md:mt-[2rem]">
+        <div className="text-lightyellow text-2xl md:text-6xl font-bold title-text mt-4 md:mt-[2rem]">
           GENESIS NFT
         </div>
         <div className="mt-4 ">
@@ -112,13 +113,13 @@ export default function Redeem() {
             Palladium
           </span>
         </div>
-        <div className="text-amber-400 text-xl lg:text-3xl font-bold mt-[2rem] md:mt-[4rem] title-text">
+        <div className="text-lightyellow text-xl lg:text-3xl font-bold mt-[2rem] md:mt-[4rem] title-text">
           MINTING IS AVAILABLE NOW
         </div>
         {isConnected ? (
           mint.toString() === "0" ? (
             <button
-              className={`w-full md:w-[15rem] bg-amber-400 text-black text-lg font-bold title-text mt-4 md:mt-[2rem] px-4 py-2 ${
+              className={`w-full md:w-[15rem] bg-lightyellow text-black text-lg font-bold title-text mt-4 md:mt-[2rem] px-4 py-2 ${
                 isPending ? "opacity-50" : ""
               }`}
               disabled={isPending}
@@ -128,17 +129,210 @@ export default function Redeem() {
               {isPending ? "Minting..." : "MINT NOW"}
             </button>
           ) : (
-            <button className="w-full md:w-[15rem] bg-amber-400 text-black text-lg font-bold title-text mt-4 md:mt-[2rem] px-4 py-2">
+            <button className="w-full md:w-[15rem] bg-lightyellow text-black text-lg font-bold title-text mt-4 md:mt-[2rem] px-4 py-2">
               ALREADY MINTED
             </button>
           )
         ) : (
           <div className="mt-4 md:mt-[2rem]">
             <CustomConnectButton />
+          
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+*/
+
+"use client";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import nftAbi from "../src/constants/abi/NftAbi.json";
+import { BOTANIX_RPC_URL } from "../src/constants/botanixRpcUrl";
+import botanixTestnet from "../src/constants/botanixTestnet.json";
+import erc20Abi from "../src/constants/abi/ERC20.sol.json";
+import { getContract } from "../src/utils/getContract";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import {
+  useAccount,
+  useTransactionReceipt,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+  useWalletClient,
+} from "wagmi";
+import web3 from "web3";
+import "../App.css";
+import Image from "next/image";
+import img from "../assets/images/Group 926.png";
+import img1 from "../assets/images/Group 906.png";
+import gif from "../assets/images/NFT.gif";
+import { CustomConnectButton } from "@/components/connectBtn";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/navigation";
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
+import { Dialog } from "primereact/dialog";
+import "./Modal.css";
+import bgimg from "../assets/images/Background.png";
+
+export default function Redeem() {
+  const { data: walletClient } = useWalletClient();
+  const { address, isConnected } = useAccount();
+  const [mint, setMint] = useState("0");
+  const [newMint, setNewMint] = useState();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const router = useRouter();
+
+  const provider = new ethers.JsonRpcProvider(BOTANIX_RPC_URL);
+
+  const nftContract = getContract(
+    "0xD8C448dD8A4785835da7af461ebB015dD83d4a12",
+    nftAbi,
+    provider
+  );
+  const mintStatus = async () => {
+    if (!address) {
+      setMint("0");
+      return;
+    }
+    const minted = await nftContract?.idOf(address);
+    setMint(minted.toString());
+  };
+
+  const { data: hash, isPending, writeContract } = useWriteContract();
+
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    });
+
+  const handleMint = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      writeContract({
+        abi: nftAbi,
+        address: "0xD8C448dD8A4785835da7af461ebB015dD83d4a12",
+        functionName: "safeMint",
+        args: [address],
+      });
+
+      router.refresh();
+    } catch (error) {
+      console.error("An error occurred", error);
+    }
+  };
+  useEffect(() => {
+    mintStatus();
+  }, [address, mint, isConfirming, isConfirmed]);
+
+  useEffect(() => {
+    if (isConfirmed) {
+      setIsModalVisible(false);
+    }
+  }, [isConfirmed]);
+
+  useEffect(() => {
+    if (isConfirming) {
+      setIsModalVisible(true);
+    }
+  }, [isConfirming]);
+
+  // mint the useEffect mai
+  const apiResponse = mint.toString();
+
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center max-sm:px-5">
+      <div className="md:mr-8 md:mt-[4rem]">
+        {/* {!isConfirmed || mint.toString() === "0" ? ( */}
+        {mint.toString() === "0" ? (
+          <Image src={img} width={600} height={450} alt="home" />
+        ) : (
+          <div className=" h-[30rem] lg:ml-56 lg:mr-10">
+            <Image src={gif} alt="home" />
+            <p className="text-lightyellow title-text text-2xl font-extrabold text-center mt-2">
+              Token ID:<span>{""}</span> {apiResponse}
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="mt-4 md:mt-[4rem]">
+        <Image
+          src={img1}
+          width={600}
+          height={33}
+          alt="home"
+          className="overflow-hidden"
+        />
+        <div className="text-lightyellow text-3xl md:text-6xl font-bold title-text mt-4 md:mt-[2rem]">
+          GENESIS NFT
+        </div>
+        <div className="mt-4 ">
+          <span className="text-white text-lg mt-4 body-text">
+            Collect the very first Circuit Breaker NFT and join the elite OGs of
+            Palladium
+          </span>
+        </div>
+
+        <div className="text-lightyellow text-xl lg:text-3xl font-bold mt-[2rem] md:mt-[4rem] title-text">
+          MINTING IS aVaILaBLE NOW
+        </div>
+        {isConnected ? (
+          // !isConfirmed ||
+          mint.toString() === "0" ? (
+            // mint.toString() === "0" ? (
+            <>
+              <button
+                className={`w-full md:w-[15rem] bg-lightyellow text-black text-lg font-bold title-text mt-4 md:mt-[2rem] px-4 py-2 ${
+                  isPending ? "opacity-50" : ""
+                }`}
+                disabled={isPending}
+                onClick={handleMint}
+                style={{ transition: "background-color 0.3s ease-in-out" }}
+              >
+                {isPending ? "Minting..." : "MINT NOW"}
+              </button>
+            </>
+          ) : (
+            <>
+              <button className="w-full md:w-[15rem] bg-lightyellow text-black text-lg font-bold title-text mt-4 md:mt-[2rem] px-4 py-2">
+                aLREADY MINTED
+              </button>
+            </>
+          )
+        ) : (
+          <div className="mt-4 md:mt-[2rem]">
+            <CustomConnectButton />
+            {/* <div className="text-white text-xl">
+              Waiting for confirmation...
+            </div> */}
+
             {/* <ConnectButton /> */}
           </div>
         )}
       </div>
+      <Dialog visible={isModalVisible} onHide={() => setIsModalVisible(false)}>
+        {isConfirming && (
+          <>
+            <div className="waiting-container">
+              <div className="waiting-message text-xl font-mono whitespace-nowrap">
+                Minting NFT ✨
+              </div>
+              <Image src={gif} className="waiting-image" alt="gif" />
+            </div>
+          </>
+        )}
+
+        {isConfirmed && (
+          <div className="text-3xl text-white">Transaction confirmed.</div>
+        )}
+      </Dialog>
     </div>
   );
 }
